6febfe151cd2e1c89acec3adc3eace2bad96cb4f
abhishekkashyap18 <117872546+abhishekkashyap18@users.noreply.github.com>
2025-02-05 22:21:32 +0530

    fix joinNewsLetter section

--- src/components/JoinNewsLetter.jsx
+++ src/components/JoinNewsLetter.jsx
@@ -13,8 +13,8 @@ function JoinNewsLetter() {
         <h1 className='text-[#37517E] font-bold text-2xl'>Join Our Newsletter</h1>
         <p className='text-center'>Subscribe to our newsletter and receive the latest news about our products and services!</p>
         <div className='bg-white drop-shadow-xl rounded-full'>
-            <input type="text" className='w-[70%] rounded-full'/>
-            <button className=' bg-[#47b2e4] text-white rounded-full p-1 sm:p-2'>Subscribe</button>
+            <input type="text" className='w-[68%] rounded-full'/>
+            <button className=' bg-[#47b2e4] text-white rounded-full p-2'>Subscribe</button>
         </div>
       </motion.div>
     </>
