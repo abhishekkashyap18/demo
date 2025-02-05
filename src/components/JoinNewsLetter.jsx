b38d8819bb55022462a73cb353f9e6c9249b0ec3
abhishekkashyap18 <117872546+abhishekkashyap18@users.noreply.github.com>
2025-02-05 22:19:17 +0530

    fix ui of joinNewsLetter section

--- src/components/JoinNewsLetter.jsx
+++ src/components/JoinNewsLetter.jsx
@@ -9,12 +9,12 @@ function JoinNewsLetter() {
       whileInView={{opacity: 1, y: 0}}
       transition = {{delay:0.5, duration: 0.9, ease: "easeInOut"}}
       viewport={{once: true}}
-      className='bg-[#f5f6f8] flex flex-col items-center justify-center p-16 mt-10 gap-5'>
+      className='bg-[#f5f6f8] flex flex-col items-center justify-center p-2 sm:p-16 mt-10 gap-5'>
         <h1 className='text-[#37517E] font-bold text-2xl'>Join Our Newsletter</h1>
         <p className='text-center'>Subscribe to our newsletter and receive the latest news about our products and services!</p>
         <div className='bg-white drop-shadow-xl rounded-full'>
-            <input type="text" className='w-60 rounded-full'/>
-            <button className=' bg-[#47b2e4] text-white rounded-full p-2'>Subscribe</button>
+            <input type="text" className='w-[70%] rounded-full'/>
+            <button className=' bg-[#47b2e4] text-white rounded-full p-1 sm:p-2'>Subscribe</button>
         </div>
       </motion.div>
     </>
